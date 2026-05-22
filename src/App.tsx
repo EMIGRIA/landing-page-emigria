dfca22ed0d8a67014e6cd16ea424dacdf1aaea90
FazrinNugraha <nugrahafadzrin@gmail.com>
2026-05-22 21:17:17 +0700

    Initial landing page files

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-center bg-amber-100 h-screen flex items-center justify-center">
+      <h1>Welcome to React with TypeScript!</h1>
+    </div>
+  );
+}
